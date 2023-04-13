2f767f22542f4cde738c09537f1fa6206794843a
JiYoon <angella990825@gmail.com>
2023-04-13 15:38:09 +0900

    2023.04.13 AddTodo(투두 추가하기) 구현

---
+++ src/components/AddTodo/AddTodo.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from 'react';
+
+export default function AddTodo({onAdd}) {
+    const [text, setText] = useState('');
+    const handleChange = (e) =>{
+        setText(e.target.value);
+    }
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        // 공백을 입력했을때는 추가되지 않도록 함
+        if(text.trim().length === 0){
+            return;
+        }
+        onAdd({id: '고유한값', text, status:'active'});
+        setText(''); // 추가하고 input 초기화
+    }
+    return (
+        // onSubmit - An Event handler function. Fires when a form gets submitted.
+        <form onSubmit = {handleSubmit}>
+            <input 
+            type='text' 
+            placeholder='Add Todo' 
+            value = {text} 
+            onChange = {handleChange}>
+            </input>
+            <button>Add</button>
+        </form>
+    );
+}
+
