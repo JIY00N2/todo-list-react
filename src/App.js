4a91bf116045b3fa4555ec4466179c44fd5076b3
JiYoon <angella990825@gmail.com>
2023-04-13 14:07:27 +0900

    2023.04.13 todo-list-react start

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './App.css';
+import TodoList from './components/TodoList/TodoList';
+
+export default function App() {
+  return (
+    <div>
+      <TodoList></TodoList>
+    </div>
+  );
+}
+
