288674963a390d24046a6de5c6b7e01472a8f92d
JiYoon <angella990825@gmail.com>
2023-04-14 17:07:11 +0900

    2023.04.14 todo item 저장

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -1,13 +1,12 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import AddTodo from '../AddTodo/AddTodo';
 import Todo from '../Todo/Todo';
 import styles from './TodoList.module.css';
 // TodoList에 필터를 받아옴
 export default function TodoList({filter}) {
-    const [todos, setTodos] = useState([
-        {id: '123', text:'장보기', status:'active'},
-        {id: '124', text:'책읽기', status:'active'},
-    ]);
+    /* useState에 내부적으로 이미 전달된 값이 있다면 또 다시 호출을 막기 위해
+    콜백함수로 감싸서 전달*/
+    const [todos, setTodos] = useState(() => readTodosFromLocalStorage());
     const handleAdd = (todo) => {
         // 새로운 todo를 todos에 업데이트 해야 함
         // setTodos를 돌면서 뒤에 todo추가
@@ -26,6 +25,11 @@ export default function TodoList({filter}) {
         todos를 돌면서 todo의 id가 삭제하고자 하는 아이템이 아닌것만 필터링*/
         setTodos(todos.filter(todo => todo.idx !== deleted.id));
     }
+    // useEffect를 이용해 localstorage에 todo 저장
+    useEffect(() => {
+        localStorage.setItem('todos', JSON.stringify(todos));
+    },[todos]);
+
     /* 필터된 아이템만 받아옴
     기존 todo와 해당하는 filter getFilteredItems 함수에 전달*/
     const filtered = getFilteredItems(todos, filter);
@@ -47,6 +51,14 @@ export default function TodoList({filter}) {
         </section>
     );
 }
+
+function readTodosFromLocalStorage(){
+    // storage에서 todo를 읽어옴
+    const todos = localStorage.getItem('todos');
+    // todo가 있다면 JSON.parse를 통해 다시 배열로 만들고 없다면 빈배열 반환
+    return todos ? JSON.parse(todos) : [];
+}
+
 /* getFilteredItems(todos, filter) 현재 todo배열과
 어떤걸 filter할 것인지 데이터를 받아옴*/
 function getFilteredItems(todos, filter){
