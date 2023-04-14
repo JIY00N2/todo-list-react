ef2bf9d38b36d4c84a69c466e5f985d6452cc689
JiYoon <angella990825@gmail.com>
2023-04-14 16:45:51 +0900

    2023.04.14 darkmode 셋팅 기억하기

--- src/context/DarkModeContext.jsx
+++ src/context/DarkModeContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useState } from "react";
+import { createContext, useContext, useEffect, useState } from "react";
 
 const DarkModeContext = createContext();
 
@@ -9,18 +9,29 @@ export function DarkModeProvider({children}){
         setDarkMode(!darkMode);
         updateDarkMode(!darkMode);
     };
+    // provider가 처음에 mount되었을때 한번 실행
+    useEffect(() =>{
+        // 다크 모드 인지 아닌지 확인, tailwind 참조
+        const isDark = 
+        localStorage.theme === 'dark' ||
+        (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme:dark)').matches);
+        setDarkMode(isDark);
+        updateDarkMode(isDark);
+    },[])
     return (
         <DarkModeContext.Provider value={{darkMode, toggleDarkMode}}>
             {children}
         </DarkModeContext.Provider>
     );
 }
-
+// html 태그에 darkMode라는 클래스 생성 및 삭제 함수
 function updateDarkMode(darkMode){
     if(darkMode){
         document.documentElement.classList.add('dark');
+        localStorage.theme ='dark';
     }else{
         document.documentElement.classList.remove('dark');
+        localStorage.theme ='light';
     }
 }
 // useDarkMode 훅을 이용
