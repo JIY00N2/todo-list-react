34d0dbdd401e30fc8276d9289cff42f005098c98
JiYoon <angella990825@gmail.com>
2023-04-14 15:40:17 +0900

    2023.04.14 header css 구현

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import styles from './Header.module.css';
+// si : style import
+// cn: className import
+
 // App에서 filters, filter, onFilterChange 전달받음
 export default function Header({filters, filter, onFilterChange}) {
     return (
-        <header>
-            <ul>
+        <header className={styles.header}>
+            <ul className={styles.filters}>
                 {/* filters를 돌면서 고정된 ui로 index를 써도 무난,
                 li의 key를 index로 지정,
                 filter의 버튼을 누르니까 button 태그 이용,
@@ -11,7 +15,11 @@ export default function Header({filters, filter, onFilterChange}) {
                 버튼이 클릭될때, 우리가 선택한 버튼의 value로 설정*/}
                 {filters.map((value,index)=>(
                 <li key={index}>
-                    <button onClick={()=>onFilterChange(value)}>{value}</button>
+                    <button
+                    // className 조합하기 -> ``이용
+                    // 현재 전달받은 필터와 내 버튼의 값이 동일하다면 styles.selected도 사용
+                    className={`${styles.filter} ${filter===value && styles.selected}`}
+                    onClick={()=>onFilterChange(value)}>{value}</button>
                 </li>))}
             </ul>
         </header>
